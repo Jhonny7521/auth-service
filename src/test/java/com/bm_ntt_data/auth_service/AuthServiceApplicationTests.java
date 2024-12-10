d655d62d6644e40b4561d54ee0253763dfd8f100
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2024-12-10 02:22:01 -0500

    :fire: First commit Auth-Service

---
+++ src/test/java/com/bm_ntt_data/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm_ntt_data.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
